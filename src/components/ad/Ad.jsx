daa2cd86fb17b785fcefd9432e231822a5752662
Dovile Bartkeviciute <d.bartkeviciute96@gmail.com>
2021-12-10 15:08:00 +0200

    style ad card better and include timeago

--- src/components/ad/Ad.jsx
+++ src/components/ad/Ad.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Icon from "../UI/Icon";
 import css from "./Ad.module.css";
+import TimeAgo from 'timeago-react';
 
 function Ad({ad, price}) {
     const [favorited, setFavorited] = useState(false);
@@ -11,14 +12,14 @@ function Ad({ad, price}) {
             <img className={css["product-image"]} src="https://media.karousell.com/media/photos/products/2018/12/05/ps3_slim_used_1543973790_3c16f9d1.jpg" alt="" />
             <div className={css["product-info"]}>
                 <h2 className={css["product-title"]}>Used PS3</h2>
-                <p><Icon icon="fa-clock-o" /> 3 years ago</p>
-                <p><Icon icon="fa-map-marker" /> Kaunas</p>
-                <p><Icon icon="fa-tag" /> Other</p>
-                <p><Icon icon="fa-eye" /> 41 views</p>
-                <div className={css["product-footer"]}>
-                    <p className={`${css["product-price"]} ${isFree ? css["free-item"] : ''}`}>{isFree ? 'Free' : itemPrice + ' €'}</p>
-                    <Icon icon={favorited ? "fa-heart" : "fa-heart-o"} onClick={() => setFavorited(!favorited)}/>
-                </div>
+                <p><Icon icon="fa-clock-o" /><TimeAgo datetime={'2021-12-10 13:00:00'}/></p>
+                <p><Icon icon="fa-map-marker" />Kaunas</p>
+                <p><Icon icon="fa-tag" />Other</p>
+                <p><Icon icon="fa-eye" />41 views</p>
+            </div>
+            <div className={css["product-footer"]}>
+                <p className={`${css["product-price"]} ${isFree ? css["free-item"] : ''}`}>{isFree ? 'Free' : itemPrice + ' €'}</p>
+                <Icon icon={favorited ? "fa-heart" : "fa-heart-o"} onClick={() => setFavorited(!favorited)}/>
             </div>
         </div>
     );
